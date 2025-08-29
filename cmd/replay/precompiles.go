5ae0af2a121b2aa02ac035073b97240de81027ad
wakabat <wakabat@protonmail.com>
2025-08-29 07:50:41 +0000

    Add dump_elf call as a precompile

---
+++ cmd/replay/precompiles.go
@@ -0,0 +1,8 @@
+// Copyright 2021-2022, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE.md
+//
+//go:build precompiles
+package main
+
+//go:wasmimport precompiles dump_elf
+func dumpElf()
